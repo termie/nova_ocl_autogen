f0f1e2559519e86c4abc3fb047bf17281cd6220c
termie <github@anarkystic.com>
2014-03-11 19:38:29 -0700

    update api db

--- build_api.py
+++ build_api.py
@@ -1,4 +1,13 @@
-#from nova.cmd import api_os_compute
+#!/usr/bin/env python
+
+"""This file attempts to build up an api definition document that OCL can use.
+
+It parses the list of calls output by parse_logs.py and uses the Nova API
+to match them to controllers.
+
+A bunch of the code to get the nova api is copied directly from the
+nova.cmd.api_os_compute code.
+"""
 
 import json
 import logging
@@ -21,21 +30,20 @@ CONF.import_opt('enabled_ssl_apis', 'nova.service')
 log = logging.getLogger()
 
 
-def main():
-    args = sys.argv
-    args.append('--config-dir=etc/nova')
-    config.parse_args(args)
-    logging.setup("nova")
-    utils.monkey_patch()
-    should_use_ssl = 'osapi_compute' in CONF.enabled_ssl_apis
-    server = service.WSGIService('osapi_compute', use_ssl=should_use_ssl)
-    print server
-    return server
-    #service.serve(server, workers=server.workers)
-    #service.wait()
+def get_server():
+  """Get the Nova API server."""
+  args = sys.argv
+  args.append('--config-dir=etc/nova')
+  config.parse_args(args)
+  logging.setup("nova")
+  utils.monkey_patch()
+  should_use_ssl = 'osapi_compute' in CONF.enabled_ssl_apis
+  server = service.WSGIService('osapi_compute', use_ssl=should_use_ssl)
+  return server
 
 
 def apirouter(server, v='/v2'):
+  """Return the ApiRouter app for the v2 api."""
   v_ref = server.app[(None, v)]
   apirouter = v_ref.application.application.application.application
   return apirouter
@@ -69,13 +77,132 @@ def to_schema(name, raw):
   return o
 
 
+def build_call_db(calls, router):
+  """Gather up all our json calls, associate them with their controller."""
+  failures = []
+  o = {}
+
+  for call in calls:
+    # we don't care about xml
+    if call['content_type'] == 'xml':
+      continue
+
+    # v3 does something weird that doesn't work with this approach
+    if call['version'] != 'v2':
+      continue
+
+    # There are some paths with extra /'s in them
+    path = call['path'].replace('//', '/')
+    # Strip the "/v2"
+    path = path[3:]
+
+    # Separate our query string
+    qs = ''
+    if '?' in path:
+      path, qs = path.split('?')
+
+    method = call['method']
+
+    match, route = match_route(method, path, qs, router)
+    if not match:
+      failures.append(('Could not match route', call))
+      continue
+
+    call_ref = normalize_call(call, match, route)
+    if not call_ref:
+      failures.append(('Could not parse call', call))
+      continue
+
+    call_ref['raw_path'] = call['path']
+    call_ref['path'] = path
+    call_ref['qs'] = qs
+
+    call_list = o.get(call_ref['name'], [])
+    call_list.append(call_ref)
+    o[call_ref['name']] = call_list
+
+  return o, failures
+
+
+def _normalize_controller_name(controller):
+  """For now just replace dots."""
+  class_name = controller.__module__
+
+  if class_name.startswith('nova.api.openstack.compute.contrib'):
+    class_name = '.'.join(class_name.split('.')[5:])
+  elif class_name.startswith('nova.api.openstack.compute'):
+    class_name = '.'.join(class_name.split('.')[4:])
+
+  return class_name.replace('.', '_')
+
+
+def match_route(method, path, qs, router):
+  environ = {'REQUEST_METHOD': method,
+             'QUERY_STRING': qs}
+
+  try:
+    match, route = router.map.routematch(path, environ=environ)
+  except Exception:
+    log.exception('Got no route for (%s, %s, %s)' % (method, path, qs))
+    return None, None
+
+  return match, route
+
+
+def normalize_call(call, match, route):
+  call = dict([(str(k), str(v)) for k, v in call.iteritems()])
+  controller = match['controller'].controller
+  class_name = _normalize_controller_name(controller)
+
+  body = call['body']
+  if call['content_type'] == 'json':
+    try:
+      body = json.loads(call['body'])
+    except Exception:
+      log.exception('Failed to parse body')
+      return None
+
+  action = match['action']
+  if action == 'index':
+    real_action = 'list'
+    name = 'list_%s' % (class_name,)
+  elif action == 'detail':
+    real_action = 'list_detail'
+    name = 'list_%s_detail' % (class_name,)
+  elif action == 'action':
+    # The real action is the only key in the top-level dict
+    real_action = body.keys()[0]
+    name = '%s_%s' % (real_action, class_name)
+  else:
+    real_action = action
+    name = '%s_%s' % (action, class_name)
+
+  route_params = [(k, v) for k, v in match.iteritems()
+                  if k not in ('action', 'controller')]
+
+  o = {'body': body,
+       'action': action,
+       'regpath': route.regpath,
+       'route_params': route_params,
+       'real_action': real_action,
+       'name': name,
+       'class_name': class_name}
+  call.update(o)
+  return call
+
+
 if __name__ == '__main__':
   calls = json.load(open('calls_from_logs.json'))
-  server = main()
-  ar_v2 = apirouter(server)
+  server = get_server()
+  router = apirouter(server)
   #ar_v3 = apirouter(server, '/v3')
 
-  #sys.exit()
+  call_db, failures = build_call_db(calls, router)
+  import pprint
+  pprint.pprint({'failures': failures, 'calls': call_db})
+
+  sys.exit()
+
   o = {}
 
   for call in calls:
